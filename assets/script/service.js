1ec2d22f5021cd9d3ec2b2d51745c6affbf94efd
CorentinPolard <corentinpolard@gmail.com>
2025-03-31 15:43:27 +0200

    Modification de la fonction showDesc, pour enlever l'erreur du message undefined qui s'affiche dans la description

--- assets/script/service.js
+++ assets/script/service.js
@@ -11,12 +11,23 @@ const descriptions = {
 }
 
 function showDesc() {
-    const service = event.target;
+    let service = event.target;
     let desc = service.children[0];
+    // Dans le cas où l'utilisateur clique sur la description (event.target = <p class="description">...</p>)
+    // On redéfinit le service pour qu'il soit égal à l'élément li de liste
+    // Et la desc qui sera maintenant égal au <p> de description
+    if (service.classList.contains('description')) {
+        service = service.parentElement;
+        desc = service.children[0];
+    }
+
+    // Si le paragraphe de description est déjà affiché on l'enlève
+    // Sinon on le construit et on l'affiche
     if (desc) {
         service.removeChild(desc);
     } else {
         desc = document.createElement('p');
+        desc.setAttribute('class', 'description');
         desc.innerText = descriptions[service.getAttribute('id')];
         service.appendChild(desc);
     }
@@ -26,27 +37,3 @@ nature.addEventListener('click', showDesc);
 evenement.addEventListener('click', showDesc);
 portrait.addEventListener('click', showDesc);
 retouche.addEventListener('click', showDesc);
-
-// nature.addEventListener('click', () => {
-//     let desc = document.querySelector("#natureDesc");
-//     if (desc) {
-//         nature.removeChild(desc);
-//     } else {
-//         let desc = document.createElement('p');
-//         desc.setAttribute('id', 'natureDesc');
-//         desc.innerText = "La photographie de la nature capture la beauté du monde sauvage à travers des images de paysages, de flore et de faune. Ce service s'adresse aux amateurs de nature, aux entreprises ou aux organismes cherchant à valoriser l'environnement. Il permet d'immortaliser des instants uniques, de sensibiliser à la préservation des écosystèmes ou d'illustrer divers projets. Grâce à une maîtrise de la lumière, du cadrage et des techniques photographiques, le photographe met en valeur la diversité et l’harmonie de la nature. Que ce soit pour des impressions artistiques, des supports de communication ou des reportages, la photographie de nature sublime le vivant.";
-//         nature.appendChild(desc);
-//     }
-// })
-
-// evenement.addEventListener('click', () => {
-//     let desc = document.querySelector("#evenementDesc");
-//     if (desc) {
-//         evenement.removeChild(desc);
-//     } else {
-//         let desc = document.createElement('p');
-//         desc.setAttribute('id', 'evenementDesc');
-//         desc.innerText = "La photographie d’événements immortalise les moments marquants de mariages, anniversaires, conférences ou spectacles. Ce service vise à capturer l’ambiance et les émotions à travers des clichés naturels et spontanés. Grâce à un œil attentif et une maîtrise des techniques de prise de vue, le photographe met en valeur chaque détail important. Idéal pour les particuliers et les entreprises souhaitant conserver un souvenir authentique ou enrichir leur communication visuelle, la photographie d’événements garantit des images de qualité, prêtes à être partagées ou imprimées pour revivre ces instants précieux.";
-//         evenement.appendChild(desc);
-//     }
-// })
