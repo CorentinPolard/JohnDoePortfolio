9defcc0d37cc9c1379836009777b2aaf61b14681
CorentinPolard <corentinpolard@gmail.com>
2025-03-31 14:19:48 +0200

    Modification de la fonction changeSlide dans slide.js pour ajouter un attribut alt aux images et augmenter le SEO

--- assets/script/slide.js
+++ assets/script/slide.js
@@ -1,30 +1,57 @@
 const slide = [
-    "assets/images/images-slide/palmiers.webp",
-    "assets/images/images-slide/drapeau.webp",
-    "assets/images/images-slide/rocher-vagues.webp",
-    // ["assets/images/images-slide/palmiers.webp", "Palmiers au pied d’un immeuble"],
-    // ["assets/images/images-slide/drapeau.webp", "Drapeau en silhouette au coucher du soleil"],
-    // ["assets/images/images-slide/rocher-vagues.webp", "Roche noire frappée par les vagues"],
+    ["assets/images/images-slide/palmiers.webp", "Palmiers au pied d’un immeuble"],
+    ["assets/images/images-slide/drapeau.webp", "Drapeau en silhouette au coucher du soleil"],
+    ["assets/images/images-slide/rocher-vagues.webp", "Roche noire frappée par les vagues"],
 ];
 
 function changeSlide(sens) {
     const img = document.querySelector("img.img-slider");
-    const imgSrc = img.getAttribute('src');
     if (img) {
-        const inArray = slide.includes(imgSrc);
-        if (inArray) {
-            let num = slide.indexOf(imgSrc);
-            num += sens;
-            if (num < 0) {
-                num = slide.length - 1;
-            } else if (num == slide.length) {
-                num = 0;
+        const imgSrc = img.getAttribute('src');
+        for (let el of slide) {
+            const inArray = el.includes(imgSrc);
+            if (inArray) {
+                let num = slide.indexOf(el);
+                num += sens;
+                if (num < 0) {
+                    num = slide.length - 1;
+                } else if (num == slide.length) {
+                    num = 0;
+                }
+                img.setAttribute('src', slide[num][0]);
+                img.setAttribute('alt', slide[num][1]);
             }
-            img.setAttribute('src', slide[num]/*[0]*/);
-            // img.setAttribute('alt', slide[num][1]);
         }
     }
 }
+
+// const slide = [
+//     "assets/images/images-slide/palmiers.webp",
+//     "assets/images/images-slide/drapeau.webp",
+//     "assets/images/images-slide/rocher-vagues.webp",
+//     // ["assets/images/images-slide/palmiers.webp", "Palmiers au pied d’un immeuble"],
+//     // ["assets/images/images-slide/drapeau.webp", "Drapeau en silhouette au coucher du soleil"],
+//     // ["assets/images/images-slide/rocher-vagues.webp", "Roche noire frappée par les vagues"],
+// ];
+
+// function changeSlide(sens) {
+//     const img = document.querySelector("img.img-slider");
+//     if (img) {
+//     const imgSrc = img.getAttribute('src');
+//         const inArray = slide.includes(imgSrc);
+//         if (inArray) {
+//             let num = slide.indexOf(imgSrc);
+//             num += sens;
+//             if (num < 0) {
+//                 num = slide.length - 1;
+//             } else if (num == slide.length) {
+//                 num = 0;
+//             }
+//             img.setAttribute('src', slide[num]/*[0]*/);
+//             // img.setAttribute('alt', slide[num][1]);
+//         }
+//     }
+// }
 
 
 setInterval(changeSlide, 5000, 1);
